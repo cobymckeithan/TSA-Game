c780073ce31dcd5c8198b5a26225d70b4f68ba94
9309860 <9309860@H0552D001967HR.wcpschools.wcpss.local>
2017-10-23 12:44:34 -0400

    loop is now in main class and were using jre 1.8 because the project yelled at me even though some computers might not have it (cough cough wcpss)

--- src/main/MainGameLoop.java
+++ src/main/MainGameLoop.java
@@ -1,12 +1,30 @@
 package main;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+
 import render.Display;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
+		// Print LWJGL version
+		System.out.println("Running LWJGL " + Version.getVersion());
+		// Create a display
 		Display d = new Display();
-		d.run();
+		
+		// Run the rendering loop until the user has attempted to close
+		// the window or has pressed the ESCAPE key
+		while (!GLFW.glfwWindowShouldClose(d.window)) {
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // Clear the window
+			GLFW.glfwSwapBuffers(d.window); // Pop it out of memory and on the screen
+			// Poll for GLFW events
+			GLFW.glfwPollEvents();
+		}
+		
+		// Destroy the window and clean up memory
+		d.destroy();
 	}
 	
 }
